c969ac6aa1b270d382846842ffdb4b859a5ace52
Srividhya Varadhan <100423362+Srividhya0504@users.noreply.github.com>
2023-05-19 17:26:53 +0530

    Update main.tf

--- reverse_shell/main.tf
+++ reverse_shell/main.tf
@@ -2,12 +2,12 @@ provider "aws"  {
   region = "${var.region}"
 }
 
-resource "aws_vpc" "main" {
+/* resource "aws_vpc" "main" {
   cidr_block = "${var.vpc_cidr}"
   tags = {
     Name = "tmp_vulnado_rev_shell_vpc"
   }
-}
+} */
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
